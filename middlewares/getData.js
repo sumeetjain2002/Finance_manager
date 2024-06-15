514e890077208a1cfc6f0fda6eeabacf22fd3f46
“sumeetjain2002” <sumeetjain.com01@gmail.com>
2024-06-15 12:46:10 +0530

    new middleware added

---
+++ middlewares/getData.js
@@ -0,0 +1,13 @@
+const transactions = require('../models/transaction');
+
+module.exports = async function getData(req, res, email) {
+    try {
+        const data = await transactions.find({email});
+       arr=data[0].statement.map((item)=>{
+        return item;
+       })
+       console.log("array ki items ------------------>", arr);
+    } catch (err) {
+        console.log(err);
+    }
+}
